83e51069f08d1bacb2ed2904ac5d115e570b3510
GracyDinma <okohagchidinma@gmail.com>
2024-01-16 11:37:54 -0800

    print c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - c program that prints
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle.\n");
+		return (0);
+}
